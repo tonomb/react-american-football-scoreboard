88a433e4a6759280850e8eece32a5aa6d85f2658
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-08 16:49:50 -0500

    refactored buttons component to buttons.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import BottomRow from "./BottomRow";
+import Buttons from "./Buttons";
 import "./App.css";
 
 function App() {
@@ -42,18 +43,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={homeTouchdown} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={homeFieldGoal} className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={awayTouchdown} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={awayFieldGoal}className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+      <Buttons homeFieldGoal={homeFieldGoal} awayFieldGoal={awayFieldGoal} homeTouchdown={homeTouchdown} awayTouchdown={awayTouchdown} />
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function Buttons (){
-
+function Buttons(props){
+    const {homeTouchdown, homeFieldGoal, awayFieldGoal, awayTouchdown} = props;
 
     return (
-        
+        <section className="buttons">
+        <div className="homeButtons">
+
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button onClick={homeTouchdown} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={homeFieldGoal} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="awayButtons">
+          <button onClick={awayTouchdown} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={awayFieldGoal}className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+      </section>
     )
 }
+
+export default Buttons;
